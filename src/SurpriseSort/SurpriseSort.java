90d93d22c5f79505af8470eeed675567ee372a56
Tiệm tạp hóa thời gian <37824359+timegrocery@users.noreply.github.com>
2021-12-06 12:14:44 +0700

    Done surprise sort

--- src/SurpriseSort/SurpriseSort.java
+++ src/SurpriseSort/SurpriseSort.java
@@ -3,28 +3,24 @@ package SurpriseSort;
 import java.util.ArrayList;
 
 public class SurpriseSort {
-    public static ArrayList<Integer> SurpriseSort (ArrayList<Integer> list){
-        for (int i = 1; i< list.size(); i++) {
-            for(int j = 1; j < list.size(); j++) {
-                if (list.get(i) < list.get(j)) {
-                    int temp = list.get(i);
-                    list.set(i, list.get(j));
-                    list.set(j, temp);
+    public static void SurpriseSort (int[] list){
+        for (int i = 0; i < list.length; i++) {
+            for(int j = 0; j < list.length; j++) {
+                if (list[i] < list[j]) {
+                    int temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
                 }
             }
         }
-        return list;
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> example = new ArrayList<>();
-        example.add(7);
-        example.add(2);
-        example.add(10);
-        example.add(20);
-        example = SurpriseSort(example);
-        for (Integer i: example) {
-            System.out.println(i);
+        int example[] = { 7, 2, 10, 20, 3, 50, 6, 40, 5, 35};
+        SurpriseSort(example);
+
+        for (int i = 0; i < example.length; i++) {
+            System.out.println(example[i]);
         }
     }
 }
